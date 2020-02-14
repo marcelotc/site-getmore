5bb2367510fe7899dda24ce423d676db742e4919
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-02-13 21:00:19 -0300

    carousel

--- _javascript/functions.js
+++ _javascript/functions.js
@@ -4,3 +4,26 @@ function mudaFoto(foto) {
 	//$('.active').removeClass('active');
 	$(this).addClass('active');
 }
+
+
+/*====================================================
+                        TESTIMONIALS
+====================================================*/
+$(function () {
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 0,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 3
+			}
+		}
+	})
+});
